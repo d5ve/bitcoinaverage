b61bc10fdfa10582fdec19f711f86a124a7123b5
Dave Webb <github@d5ve.com>
2016-03-03 08:52:53 +1300

    Use Getopt::Long and Pod::Usage. Add POD

--- bitcoinaverage.pl
+++ bitcoinaverage.pl
@@ -8,27 +8,43 @@ use v5.10;
 
 bitcoinaverage.pl - fetch bitcoin pricing info from api.bitcoinaverage.com
 
+=head1 SYNOPSIS
+
+    bitcoinaverage.pl [options]
+
+    Options:
+        --primary_currency=     Defaults to USD.
+        --local_currency=       Defaults to NZD.
 =cut
 
+use Getopt::Long;
 use HTTP::Tiny;
 use JSON;
+use Pod::Usage;
 
-my $ticker_url       = q{https://api.bitcoinaverage.com/ticker/all};
-my $primary_currency = 'USD';
-my $local_currency   = 'NZD';
-my $output_field     = '24h_avg';
+my $ticker_url          = q{https://api.bitcoinaverage.com/ticker/all};
+my $history_url_format  = q{https://api.bitcoinaverage.com/history/%s/per_day_all_time_history.csv};
+my $primary_currency    = 'USD';
+my $local_currency      = 'NZD';
+my $ticker_output_field = '24h_avg';
 
-my $response = HTTP::Tiny->new->get($ticker_url);
+GetOptions(
+    'primary_currency=s' => \$primary_currency,
+    'local_currency=s'   => \$local_currency,
+    'help|?'             => sub { pod2usage() },
+) or pod2usage();
 
-if ( !$response->{success} ) {
-    say "Unable to GET '$ticker_url': $response->{status} $response->{reason}";
+my $ticker_response = HTTP::Tiny->new->get($ticker_url);
+
+if ( !$ticker_response->{success} ) {
+    say "Unable to GET '$ticker_url': $ticker_response->{status} $ticker_response->{reason}";
     exit;
 }
 
-my $data = JSON::decode_json( $response->{content} );
+my $ticker_data = JSON::decode_json( $ticker_response->{content} );
 
-say "$primary_currency: $data->{$primary_currency}->{$output_field}";
-say "$local_currency: $data->{$local_currency}->{$output_field}" if $primary_currency ne $local_currency;
+say "$primary_currency: $ticker_data->{$primary_currency}->{$ticker_output_field}";
+say "$local_currency: $ticker_data->{$local_currency}->{$ticker_output_field}" if $primary_currency ne $local_currency;
 
 exit;
 
